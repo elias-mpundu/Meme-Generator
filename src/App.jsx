d1a991f29ea5d11e36b97da246b88ad91bd9026b
1102180027 <elias.mpundu@outlook.com>
2023-12-12 06:19:50 -0600

    Complete project - React App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "./components/header.jsx"
+import Meme from "./components/form.jsx"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header id="header">
+            <h1>Meme Generator App</h1> 
+            <p>Elias Mpundu</p>
+        </header>
+    )
+}
